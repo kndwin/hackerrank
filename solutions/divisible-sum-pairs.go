b13054f019b199f1ec157ba3d97fc988edf78319
Kevin Nguyen <me@kndwin.dev>
2020-09-03 14:51:52 +1000

    Create divisible-sum-pairs.go
    Solution for "Divisible Sum Pairs" in go

---
+++ solutions/divisible-sum-pairs.go
@@ -0,0 +1,21 @@
+// Complete the divisibleSumPairs function below.
+
+func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
+    i := int32(0)
+    ans := int32(0)
+
+    for i < n {
+        j := int32(0)
+        curr := ar[i]
+        ar := ar[i+1:]
+        for  j < int32(len(ar)) {
+            if pair := curr+ar[j]; pair%k == 0 {
+                ans++
+            }
+            j++   
+        }
+        i++
+    }
+
+    return ans;
+}
